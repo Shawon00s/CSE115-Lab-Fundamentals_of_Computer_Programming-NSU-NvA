5ac4c531ad52cb47a1334c8d90282be2c52a2fb4
Sudipto Roy Shawon <sudipto.roy@northsouth.edu>
2024-01-25 18:05:03 +0600

    Update Task2.c

--- Manual-1–Introduction/Task2.c
+++ Manual-1–Introduction/Task2.c
@@ -1,3 +1,15 @@
+/*
+    Write a program that reads in 3 numbers and prints their average.
+
+        Input:
+        Insert first number: 3 
+        Insert second number: 8 
+        Insert third number: 2
+
+        Output:
+        The average is: 4.3
+*/
+
 #include<stdio.h>
 
 int main()
