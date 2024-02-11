feccbdc43e52ef6d6c2603d26514eb1dfb08d528
Sudipto Roy Shawon <sudipto.roy@northsouth.edu>
2024-02-11 20:52:53 +0600

    Add files via upload

---
+++ Manual-4-if_else part1/Exercises2.c
@@ -0,0 +1,23 @@
+/*
+    2. Write a C program that decides whether a person is eligible to work in a particular company or not.
+    The company policy is: If the person’s age is between 25 and 45, s/he are eligible to work.
+    Otherwise, your software should say “You are too young or too old”.
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int age;
+    printf("Enter your age: ");
+    scanf("%d",&age);
+
+    if(age >= 25 && age <= 45)
+    {
+        printf("Eligible to work");
+    }
+    else
+    {
+        printf("You are too young or too old");
+    }
+}
